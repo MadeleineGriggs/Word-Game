52627e8a2f49197ae51207ee9954b49d6ea703a3
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-05-31 14:01:43 -0400

    added function for selecting random word from array and displaying the length of the word as underscores

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -10,7 +10,9 @@ var wordToGuess = wordArray[Math.floor(Math.random() * wordArray.length)]
 
 function displayUnderscores() {
     var wordLength = wordToGuess.length;
-    console.log(wordLength);
+    var underScores = document.getElementById("word_to_guess_container");
+    var letter = "_ "
+    underScores.innerHTML = letter.repeat(wordLength);
 }
 
 function getContainers(x) {
