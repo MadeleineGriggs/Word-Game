5ef4af0ee8b6b76692882c8cdb364a8a91496fe9
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-06-03 15:02:03 -0400

    code for underscoreArray, rewrote checkLetters function so it replaces the underscores with the appropriate letter

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -6,34 +6,44 @@ var wordArray = [
 ]
 
 // selects a random word from the wordArray
-var wordToGuess = wordArray[Math.floor(Math.random() * wordArray.length)]
+var wordToGuess = wordArray[Math.floor(Math.random() * wordArray.length)];
+// An array to store the underscores that represent the missing letters.
+var underscoreArray = [];
+// fills the UnderscoreArray with the correct number of underscores for the randomly selected word.
+for (i = 0; i < wordToGuess.length; i++) {
+    underscoreArray[i] = "_"; 
+}
 
-// displays the correct number of blank spaces for the word that was randomly selected
+var lettersRemaining = wordToGuess.length;
+
+// displays the correct number of blank spaces for the word that was randomly selected. Also adds a space between each underscore.
 function displayUnderscores() {
-    var wordLength = wordToGuess.length;
     var underScores = document.getElementById("word_to_guess_container");
-    var letter = "_ "
-    underScores.innerHTML = letter.repeat(wordLength);
+    underScores.innerHTML = underscoreArray.join(" ");
 }
 
-function checkLetters(letterGuessed) {
-    var userLetter = letterGuessed.key;
-    console.log("You guessed: " + userLetter);
-    // if (letterGuessed.keyCode == /^[A-Za-z]+$/) {
-    //     console.log("this is working?");
-    // } else {
-    //     console.log("this isn't working");
-    // }
-}
+    function checkLetters(keyPressed) {
+        var userLetter = keyPressed.key;
+        for( a = 0; a < wordToGuess.length ; a++) {
+            if (wordToGuess[a] == userLetter) {
+                underscoreArray[a] = userLetter;
+                lettersRemaining--;
+            } else if (wordToGuess[a] !== userLetter){
+                console.log("This letter doesn't match.");
+            }
+        }
+        displayUnderscores();
+    }
 
 function getContainers(x) {
     var element = document.getElementById(x);
     element.innerHTML = wordToGuess;
 }
 
+var letters = /^[A-Za-z]+$/;
 
 function logKey(keyPressed) {
-  if (keyPressed.keyCode == 87){
+  if (keyPressed.keyCode == letters){
     getContainers("wins_container");
   }
 }
@@ -41,5 +51,25 @@ function logKey(keyPressed) {
 // Event Listeners
 
 document.addEventListener('keyup', logKey);
-document.addEventListener('keyup', displayUnderscores);
 document.addEventListener('keyup', checkLetters);
+// document.addEventListener('keyup', displayUnderscores);
+
+
+// function checkLetters(letterGuessed) {
+//     var userLetter = letterGuessed.key;
+//     console.log("You guessed: " + userLetter);
+//     if (wordToGuess.includes(userLetter)) {
+
+//         for (var i = 0; i < wordToGuess.length; i++) {
+//             var letterCorrect = wordToGuess.charAt(i);
+//             if (wordToGuess.includes(letterCorrect)) {
+//                 console.log(wordToGuess.charAt(i));
+//             } else {
+//                 console.log("This letter does not appear.");
+//             }
+//           }
+//         console.log("the letter you picked is contained in the word.");
+//     } else {
+//         console.log("Not sure...");
+//     }
+// }
